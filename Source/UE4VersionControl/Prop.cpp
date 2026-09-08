0a76765a9ce3323dd0c3f38d281251f610c90d5f
Subhronil Ray <sray76382@gmail.com>
2026-09-08 22:27:02 +0530

    Added mesh to Prop, Created BP_Prop

--- Source/UE4VersionControl/Prop.cpp
+++ Source/UE4VersionControl/Prop.cpp
@@ -8,14 +8,14 @@ AProp::AProp()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-
+	 MeshComp = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("MeshComp"));
+	 SetRootComponent(MeshComp);
 }
 
 // Called when the game starts or when spawned
 void AProp::BeginPlay()
 {
 	Super::BeginPlay();
-	
 }
 
 // Called every frame
